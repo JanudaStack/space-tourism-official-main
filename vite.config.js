e28f8e9d892a3ed7a7aefc8c51e11cd5ffd6f278
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-06-11 22:19:40 +0530

    Update base path in Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { ghPages } from 'vite-plugin-gh-pages';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), ghPages()],
-    base: '/space-tourism-react-main/', // Replace with your repository name
+    base: '/space-tourism-official-main/', // Replace with your repository name
 });
