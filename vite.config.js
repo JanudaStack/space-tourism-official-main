dc86a8983cacb039f11f5c77a43adffa27cad00c
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-06-11 22:24:54 +0530

    chore: update deployment scripts and dependencies
    - Changed the deploy script to use 'gh-pages' for deployment.
    - Added 'predeploy' script to build the project before deployment.
    - Updated package.json to include 'gh-pages' as a dependency.
    - Removed 'vite-plugin-gh-pages' from Vite configuration and adjusted plugins.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), ghPages()],
-    base: '/space-tourism-official-main/', // Replace with your repository name
+    plugins: [react()],
+    base: '/space-tourism-official-main/', // Replace <repository-name> with your repo name
 });
