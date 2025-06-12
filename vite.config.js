d814ed4b013e8eb1b719164ced7d11e86aaa3386
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-06-13 01:47:39 +0530

    fix: update routing to use HashRouter and correct Vite base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/space-tourism-official-main/', // Replace <repository-name> with your repo name
+    base: '/space-tourism-react-main/', // Set this to your repository name
 });
