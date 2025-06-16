02ea1fc61171bed85b7d006a240276c2c3473c0a
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-06-16 23:35:35 +0530

    added crewData to dynamic visualization

--- src/pages/Crew/Crew.jsx
+++ src/pages/Crew/Crew.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Crew.module.css'; // Import CSS module
-import CrewImageAnoushehAnsari from '../../assets/crew/image-anousheh-ansari.webp';
+import CrewImageCommander from '../../assets/crew/image-douglas-hurley.webp';
+import CrewImageEngineer from '../../assets/crew/image-anousheh-ansari.webp';
+import CrewImagePilot from '../../assets/crew/image-victor-glover.webp';
+import CrewImageSpecialist from '../../assets/crew/image-mark-shuttleworth.webp';
+
+const crewData = [
+    {
+        title: 'Commander',
+        name: 'Douglas Hurley',
+        image: CrewImageCommander,
+        info: 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.',
+    },
+    {
+        title: 'Flight Engineer',
+        name: 'Anousheh Ansari',
+        image: CrewImageEngineer,
+        info: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.',
+    },
+    {
+        title: 'Pilot',
+        name: 'Victor Glover',
+        image: CrewImagePilot,
+        info: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.',
+    },
+    {
+        title: 'Mission Specialist',
+        name: '  Mark Shuttleworth',
+        image: CrewImageSpecialist,
+        info: 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.',
+    },
+];
 
 const Crew = () => {
+    const [selectedMember, setSelectedMember] = useState(crewData[0]);
+
     return (
         <>
             <main>
@@ -17,28 +49,23 @@ const Crew = () => {
                             <div className={styles['crew-content']}>
                                 <div className={styles['crew-header']}>
                                     <h3 className={styles['crew-title']}>
-                                        Flight Engineer
+                                        {selectedMember.title}
                                     </h3>
                                     <h2 className={styles['crew-name']}>
-                                        Anousheh Ansari
+                                        {selectedMember.name}
                                     </h2>
                                 </div>
                                 <div className={styles['crew-info']}>
                                     <p id={styles['crew-info']}>
-                                        Anousheh Ansari is an Iranian American
-                                        engineer and co-founder of Prodea
-                                        Systems. Ansari was the fourth
-                                        self-funded space tourist, the first
-                                        self-funded woman to fly to the ISS, and
-                                        the first Iranian in space.
+                                        {selectedMember.info}
                                     </p>
                                 </div>
                                 <div className={styles['crew-nav']}></div>
                             </div>
                             <picture className={styles['crew-img']}>
                                 <img
-                                    src={CrewImageAnoushehAnsari}
-                                    alt="Anousheh Ansari"
+                                    src={selectedMember.image}
+                                    alt={selectedMember.name}
                                 />
                             </picture>
                         </div>
