2f9fe651682480e49a3f2ba0853c9f1c69d43fb1
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-06-22 01:47:52 +0530

    feat: toggle overflow-x style on menu open/close in Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import hamburgerIcon from '../../assets/shared/icon-hamburger.svg';
-import siteLogo from '../../assets/shared/logo.svg';
 import closeIcon from '../../assets/shared/icon-close.svg';
+import siteLogo from '../../assets/shared/logo.svg';
 import './Navbar.css';
 
 function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => setMenuOpen(!menuOpen);
-    const closeMenu = () => setMenuOpen(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+        const siteHeader = document.querySelector('.site-header');
+        siteHeader.style.overflowX = menuOpen ? 'hidden' : 'visible'; // Toggle overflow-x
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+        const siteHeader = document.querySelector('.site-header');
+        siteHeader.style.overflowX = 'hidden'; // Set overflow-x to hidden when menu is closed
+    };
 
     return (
         <header className="site-header">
@@ -19,43 +28,15 @@ function Navbar() {
                         <img src={siteLogo} alt="site-logo" />
                     </Link>
                 </div>
-                <div className="tablet-navbar">
-                    <ul className="nav-links-tablet">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/destination">Destination</Link>
-                        </li>
-                        <li>
-                            <Link to="/crew">Crew</Link>
-                        </li>
-                        <li>
-                            <Link to="/technology">Technology</Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="desktop-navbar">
-                    <ul className="nav-links-desktop">
-                        <li>
-                            <Link to="/">00 Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/destination">01 Destination</Link>
-                        </li>
-                        <li>
-                            <Link to="/crew">02 Crew</Link>
-                        </li>
-                        <li>
-                            <Link to="/technology">03 Technology</Link>
-                        </li>
-                    </ul>
-                </div>
                 <div className="menu-btn-mobile">
                     <button className="hamburger-menu" onClick={toggleMenu}>
                         <img
                             src={menuOpen ? closeIcon : hamburgerIcon}
-                            alt="menu-icon"
+                            alt={
+                                menuOpen
+                                    ? 'close-menu-icon'
+                                    : 'hamburger-menu-icon'
+                            }
                         />
                     </button>
                 </div>
