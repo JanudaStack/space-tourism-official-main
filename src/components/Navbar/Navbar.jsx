627ed0e5c008a1911dbb8b2b106c96351f7adef7
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-06-22 01:55:53 +0530

    feat: add tablet and desktop navigation menus to Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,6 +28,38 @@ function Navbar() {
                         <img src={siteLogo} alt="site-logo" />
                     </Link>
                 </div>
+                <div className="tablet-navbar">
+                    <ul className="nav-links-tablet">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/destination">Destination</Link>
+                        </li>
+                        <li>
+                            <Link to="/crew">Crew</Link>
+                        </li>
+                        <li>
+                            <Link to="/technology">Technology</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="desktop-navbar">
+                    <ul className="nav-links-desktop">
+                        <li>
+                            <Link to="/">00 Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/destination">01 Destination</Link>
+                        </li>
+                        <li>
+                            <Link to="/crew">02 Crew</Link>
+                        </li>
+                        <li>
+                            <Link to="/technology">03 Technology</Link>
+                        </li>
+                    </ul>
+                </div>
                 <div className="menu-btn-mobile">
                     <button className="hamburger-menu" onClick={toggleMenu}>
                         <img
