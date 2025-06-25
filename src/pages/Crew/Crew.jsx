8a36d66396e5f92b48daf3cef5d84a688d9ed7d7
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-06-25 23:18:50 +0530

    style: add unique IDs to crew members and refactor crew navigation for improved functionality

--- src/pages/Crew/Crew.jsx
+++ src/pages/Crew/Crew.jsx
@@ -7,24 +7,28 @@ import CrewImageSpecialist from '../../assets/crew/image-mark-shuttleworth.webp'
 
 const crewData = [
     {
+        id: 1,
         title: 'Commander',
         name: 'Douglas Hurley',
         image: CrewImageCommander,
         info: 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.',
     },
     {
+        id: 2,
         title: 'Flight Engineer',
         name: 'Anousheh Ansari',
         image: CrewImageEngineer,
         info: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.',
     },
     {
+        id: 3,
         title: 'Pilot',
         name: 'Victor Glover',
         image: CrewImagePilot,
         info: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.',
     },
     {
+        id: 4,
         title: 'Mission Specialist',
         name: '  Mark Shuttleworth',
         image: CrewImageSpecialist,
@@ -34,6 +38,13 @@ const crewData = [
 
 const Crew = () => {
     const [selectedMember, setSelectedMember] = useState(crewData[0]);
+    // const [isTransitioning, setIsTransitioning] = useState(false);
+
+    const handleCrewChange = (crew) => {
+        setTimeout(() => {
+            setSelectedMember(crew);
+        }, 100);
+    };
 
     return (
         <>
@@ -62,18 +73,15 @@ const Crew = () => {
                                 </div>
                                 <div className={styles['crew-nav']}>
                                     <ul className={styles['crew-nav-dots']}>
-                                        <li>
-                                            <button></button>
-                                        </li>
-                                        <li>
-                                            <button></button>
-                                        </li>
-                                        <li>
-                                            <button></button>
-                                        </li>
-                                        <li>
-                                            <button></button>
-                                        </li>
+                                        {crewData.map((crew) => (
+                                            <li key={crew.id}>
+                                                <button
+                                                    onClick={() =>
+                                                        handleCrewChange(crew)
+                                                    }
+                                                ></button>
+                                            </li>
+                                        ))}
                                     </ul>
                                 </div>
                             </div>
