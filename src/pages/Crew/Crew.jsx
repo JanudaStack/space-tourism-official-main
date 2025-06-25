9ed1d0ac78b105579591af327d7898d0a796a6c1
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-06-25 23:28:37 +0530

    style: implement fade-in and fade-out animations for crew member transitions and refactor CSS class names for consistency

--- src/pages/Crew/Crew.jsx
+++ src/pages/Crew/Crew.jsx
@@ -25,12 +25,12 @@ const crewData = [
         title: 'Pilot',
         name: 'Victor Glover',
         image: CrewImagePilot,
-        info: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.',
+        info: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18. He was a crew member of Expedition 64, and served as a station systems flight engineer.',
     },
     {
         id: 4,
         title: 'Mission Specialist',
-        name: '  Mark Shuttleworth',
+        name: 'Mark Shuttleworth',
         image: CrewImageSpecialist,
         info: 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.',
     },
@@ -38,12 +38,14 @@ const crewData = [
 
 const Crew = () => {
     const [selectedMember, setSelectedMember] = useState(crewData[0]);
-    // const [isTransitioning, setIsTransitioning] = useState(false);
+    const [isTransitioning, setIsTransitioning] = useState(false);
 
     const handleCrewChange = (crew) => {
+        setIsTransitioning(true); // Start transition
         setTimeout(() => {
-            setSelectedMember(crew);
-        }, 100);
+            setSelectedMember(crew); // Update the selected member
+            setIsTransitioning(false); // End transition
+        }, 500); // Match timeout with CSS animation duration
     };
 
     return (
@@ -57,7 +59,13 @@ const Crew = () => {
                     </div>
                     <div className={styles['crew-content-wrapper']}>
                         <div className={styles['crew-container']}>
-                            <div className={styles['crew-content']}>
+                            <div
+                                className={`${styles['crew-content']} ${
+                                    isTransitioning
+                                        ? styles['fade-out']
+                                        : styles['fade-in']
+                                }`}
+                            >
                                 <div className={styles['crew-header']}>
                                     <h3 className={styles['crew-title']}>
                                         {selectedMember.title}
@@ -67,7 +75,7 @@ const Crew = () => {
                                     </h2>
                                 </div>
                                 <div className={styles['crew-info']}>
-                                    <p id={styles['crew-info']}>
+                                    <p className={styles['crew-info-text']}>
                                         {selectedMember.info}
                                     </p>
                                 </div>
@@ -78,6 +86,14 @@ const Crew = () => {
                                                 <button
                                                     onClick={() =>
                                                         handleCrewChange(crew)
+                                                    }
+                                                    className={
+                                                        selectedMember.id ===
+                                                        crew.id
+                                                            ? styles[
+                                                                  'active-dot'
+                                                              ]
+                                                            : ''
                                                     }
                                                 ></button>
                                             </li>
